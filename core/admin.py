bfca604f37e93cf27676b62530969f958cd67ef9
singhg9876 <singhgaurav9876@gmail.com>
2022-04-03 23:03:57 +0530

    d

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from core.models import Record
-
-
-@admin.register(Record)
-class RecordAdmin(admin.ModelAdmin):
-    list_display = ("id", "language", "voice_record")
